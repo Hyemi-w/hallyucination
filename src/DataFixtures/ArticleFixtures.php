8b0a5a1a77a258e5c2167f4f53117e4a75bfea0a
Elodie Billon <elodie.billon1995@outlook.fr>
2019-07-11 13:27:21 +0200

    fixtures crées

---
+++ src/DataFixtures/ArticleFixtures.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wilder9
+ * Date: 11/07/19
+ * Time: 13:04
+ */
+
+namespace App\DataFixtures;
+
+
+use App\Entity\Article;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Faker;
+
+class ArticleFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $faker = Faker\Factory::create('fr_FR');
+        for ($i=0; $i<=3; $i++) {
+            $article = new Article();
+            $article->setTitle($faker->sentence);
+            $article->setContent($faker->text);
+            $article->setPicture($faker->imageUrl($width=400, $height = 200));
+            $manager->persist($article);
+        }
+        $manager->flush();
+    }
+}
