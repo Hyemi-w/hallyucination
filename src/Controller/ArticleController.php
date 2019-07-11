733095b3fcd53070b3546b91d9a54c2fe6813a57
Elodie Billon <elodie.billon1995@outlook.fr>
2019-07-11 17:15:56 +0200

    Controller created, datas imported

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ArticleRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController extends AbstractController
+{
+    /**
+     * @param ArticleRepository $articleRepository
+     * @Route("/articles", name="articles-index")
+     * @return Response
+     */
+    public function index(ArticleRepository $articleRepository) : Response
+    {
+        return $this->render('article/index.html.twig', [
+            'articles' => $articleRepository->findBy([], ['date' => 'DESC'])
+        ]);
+    }
+}
